38416f25ba260962319b057681b62f6303620ba4
Rubin Simons <rubin@raaf.tech>
2018-01-13 20:02:47 +0100

    Tumdidum..

--- test.c
+++ test.c
@@ -1,22 +1,40 @@
 #include "conio.h"
+#include "nes.h"
+#include "stdio.h"
 
-void main()
+void delay(int i)
 {
-    unsigned int s = 0;
-    int i = 0;
-
-    cprintf("6502-C Compiler. \r\nhttp://HelloACM.com\r\n");
-
-    cprintf("Size Of int = %d\r\n", sizeof(int));
-    cprintf("Size Of char = %d\r\n", sizeof(char));
-    cprintf("Size Of float = %d\r\n", sizeof(float));
-    cprintf("Size Of double = %d\r\n", sizeof(double));
-    cprintf("Size Of short = %d\r\n", sizeof(short));
-
-    for (i = 0; i <= 100; i ++) {
-        s += i;
+    while(i--){
+        waitvsync();
     }
-    cprintf("Sum = %d\r\n", s);
+}
 
-    for(;;); // loop forever, never ends
+void main() {
+    unsigned char wx, wy;
+    char oy, i, j, k;
+    /* use const to save memory */
+    const char s[][24] = {"Scroll Test", "Blah line 1\0", " Blah line 2\0", "Blah line 3\0"};
+    screensize(&wx, &wy); // get screen size (width and height)
+    bgcolor(COLOR_BLUE);
+    textcolor(COLOR_WHITE);
+    clrscr(); // clean the screen
+    i = wy - 4;
+    oy = i;
+    for (;;) {
+        delay(50); // 50ms
+        for (j = 0; j < 4; j ++) { // clean previous text
+            gotoxy(0, oy + j);
+            for (k = 0; k < wx; k ++) {
+                cputc(32); // print a whitespace
+            }
+        }
+        if (--i < 1) {
+            i = wy - 4;
+        }
+        for (j = 0; j < 4; j ++) {
+            gotoxy(0, i + j);
+            cputs(s[j]);
+        }
+        oy = i;
+    }
 }
