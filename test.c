3ec078e25bc0afdb71c9641348286e79f3ee65de
Rubin Simons <rubin@raaf.tech>
2018-01-13 20:15:35 +0100

    Writing to VRAM.

--- test.c
+++ test.c
@@ -1,40 +1,13 @@
 #include "conio.h"
-#include "nes.h"
-#include "stdio.h"
 
-void delay(int i)
+typedef unsigned char u8;
+#define address(add) (*(u8 *)(add))
+
+void main()
 {
-    while(i--){
-        waitvsync();
-    }
-}
-
-void main() {
-    unsigned char wx, wy;
-    char oy, i, j, k;
-    /* use const to save memory */
-    const char s[][24] = {"Scroll Test", "Blah line 1\0", " Blah line 2\0", "Blah line 3\0"};
-    screensize(&wx, &wy); // get screen size (width and height)
-    bgcolor(COLOR_BLUE);
-    textcolor(COLOR_WHITE);
-    clrscr(); // clean the screen
-    i = wy - 4;
-    oy = i;
-    for (;;) {
-        delay(50); // 50ms
-        for (j = 0; j < 4; j ++) { // clean previous text
-            gotoxy(0, oy + j);
-            for (k = 0; k < wx; k ++) {
-                cputc(32); // print a whitespace
-            }
-        }
-        if (--i < 1) {
-            i = wy - 4;
-        }
-        for (j = 0; j < 4; j ++) {
-            gotoxy(0, i + j);
-            cputs(s[j]);
-        }
-        oy = i;
-    }
+    cprintf("visit VRAM");
+    address(0x2006) = 0x21;
+    address(0x2006) = 0xf0;
+    address(0x2007) = 65;
+    while(1);
 }
